18a92b536855dc554d4ccab8b2e60adf84d1134d
Sourav Kumar <xsouravkumar357@gmail.com>
2026-04-08 15:58:53 +0530

    main route file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,24 @@
+
+
 import express from 'express'
 import userRoutes from './user.routes.js'
 import postRoutes from './post.routes.js'
 
+
 const router = express.Router()
+
 
 router.use('/users', userRoutes)
 router.use('/posts', postRoutes)
 
+
 router.get('/health', (req, res) => {
-  res.json({ success: true, message: 'Server is healthy', data: { uptime: process.uptime() } })
+  res.json({ 
+    success: true, 
+    message: 'Server is healthy'
+  })
 })
 
+
 export default router
+
