4fa712d269018d9c3139401f7ce288b63b91ddbe
Sourav Kumar <xsouravkumar357@gmail.com>
2026-04-08 15:57:07 +0530

    app file code

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
+
+
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import routes from './routes/index.js'
 
+
 const app = express()
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use(morgan('dev'))
 
+
 app.use('/api/v1', routes)
+
 
 export default app
